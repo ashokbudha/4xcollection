e4ded7339246db2004999fbf7311b3e343eb3c66
Ashok Budha <abudha592@gmail.com>
2026-08-06 21:10:53 +0545

    error resolved: renamed home

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home/home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import ProductsPage from "../pages/ProductPage/ProductPage";
